14f355ed159b2e476956e22dbe26e4024aab9edd
Theo M <theodoremoreland@yahoo.com>
2019-04-30 21:23:17 -0500

    Fixed improper/illogical syntax, fixed reset table function, and fixed multiple filtering functionality.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,8 @@
 var tableData = data;
+const tableData2 = JSON.parse(JSON.stringify(data));
 var filter_btn = d3.select("#filter-btn");
 var tbody = d3.select("tbody");
+var reset_table = d3.select("#reset_table");
 
 filter_btn.on("click", function() {
 
@@ -12,29 +14,42 @@ filter_btn.on("click", function() {
     var state = d3.select("#state").property("value");
     var country = d3.select("#country").property("value");
     var shape = d3.select("#shape").property("value");
+    var filter = "var filtered_table = tableData";
+    
 
-    if(datetime) { new_tableData = tableData.filter(sighting => sighting.datetime === datetime); }
-    if(city) { new_tableData = tableData.filter(sighting => sighting.city === city); }
-    if(state) { new_tableData = tableData.filter(sighting => sighting.state === state); }
-    if(country) { new_tableData = tableData.filter(sighting => sighting.country === country); }
-    if(shape) { new_tableData = tableData.filter(sighting => sighting.shape === shape); }
+    if(datetime) { filter = filter.concat(".filter(sighting => sighting.datetime === datetime)"); }
+    if(city) { filter = filter.concat(".filter(sighting => sighting.city === city)"); }
+    if(state) { filter = filter.concat(".filter(sighting => sighting.state === state)"); }
+    if(country) { filter = filter.concat(".filter(sighting => sighting.country === country)"); }
+    if(shape) { filter = filter.concat(".filter(sighting => sighting.shape === shape)"); }
 
-    new_tableData.forEach(function(object_) {
+    console.log(filter);
+    eval(filter);
+
+    filtered_table.forEach(function(object_) {
         var row = tbody.append("tr");
         Object.entries(object_).forEach(function([key, value]) {
-            var cell = tbody.append("td");
+            var cell = row.append("td");
             cell.text(value);
         });
     });
 });
 
-tableData.forEach(function(object_) {
-    var row = tbody.append("tr");
-    Object.entries(object_).forEach(function([key, value]) {
-        var cell = tbody.append("td");
-        cell.text(value);        
+function populate() {
+    tableData2.forEach(function(object_) {
+        var row = tbody.append("tr");
+        Object.entries(object_).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);        
+        });
     });
+}
+
+populate();
+
+reset_table.on("click", function() {
+    d3.event.preventDefault();
+    tbody.selectAll("td").remove();
+    populate();
 });
-
-
 
